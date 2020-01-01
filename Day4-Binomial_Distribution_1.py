602fba899074e0cd72afe417b05ef066a8126cac
Hari Kiran <43600130+harikiran-mudipalli@users.noreply.github.com>
2020-01-01 22:16:49 +0530

    Update Day4-Binomial_Distribution_1.py

--- Day4-Binomial_Distribution_1.py
+++ Day4-Binomial_Distribution_1.py
@@ -1,5 +1,5 @@
 """ Task:
-The ratio of boys to girls for babies born in Russia is . If there is  child born per birth, what proportion of Russian families with exactly  children will have at least  boys?
+The ratio of boys to girls for babies born in Russia is . If there is  child born per birth, what proportion of Russian families with exactly 6 children will have at least 3 boys?
 Write a program to compute the answer using the above parameters. Then print your result, rounded to a scale of  decimal places (i.e.,  format).
 """
 P,Q = map(float,input().split(' '))
