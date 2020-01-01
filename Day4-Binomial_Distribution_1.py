767e3c6cee31d987d6da4f71ca3c07f97744ca00
Hari Kiran <hkhari019@gmail.com>
2020-01-01 22:06:25 +0530

    Create Day4-Binomial_Distribution_1.py

---
+++ Day4-Binomial_Distribution_1.py
@@ -0,0 +1,19 @@
+P,Q = map(float,input().split(' '))
+total = P+Q
+N = 6
+atleast = 3
+Sum = 0
+def combinations(n,r):
+    n1=n
+    r1=r
+    for j in range(1,r):
+        n1*=(n-j)
+        r1*=(r-j)
+    return n1/r1
+def pos(p,x):
+    return pow(p,x)
+def neg(q,n,x):
+    return pow(q,(n-x))
+for i in range(atleast,N+1):
+    Sum+=combinations(N,i)*pos(P/total,i)*neg(Q/total,N,i)
+print(round(Sum,3))
